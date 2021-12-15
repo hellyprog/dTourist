f8a7d70c6946beb978403065234cae1d1381dccd
volodymyr.romaniv <v.b.romaniv@gmail.com>
2021-12-15 23:26:16 +0200

    Customs implementation changed

--- smart-contracts/contracts/Customs.sol
+++ smart-contracts/contracts/Customs.sol
@@ -1,11 +1,22 @@
+// SPDX-License-Identifier: MIT
 pragma solidity >=0.4.22 <0.9.0;
 
 contract Customs {
-    function crossBorder(address payable _user) public {
-        bool isAllowed = _isCrossingAllowed(_user); 
+    mapping (address => Trip[]) public personToHistory;
+    mapping (address => string) public addressToCountry;
+
+    function crossBorder(address _from, address _to, uint256 _date) public {
+        personToHistory[msg.sender].push(Trip(_from, _to, _date, true));
     }
 
-    function _isCrossingAllowed(address payable _user) private returns (bool) {
+    /*function _isCrossingAllowed(address _user) private pure returns (bool) {
         return true;
+    }*/
+
+    struct Trip {
+        address from;
+        address to;
+        uint256 date;
+        bool success;
     }
 }
