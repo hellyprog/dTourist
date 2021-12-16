f24a62b492d86709db1b7a41c9e6609f7f2aef07
volodymyr.romaniv <v.b.romaniv@gmail.com>
2021-12-16 23:09:43 +0200

    Added country initialization

--- smart-contracts/contracts/Customs.sol
+++ smart-contracts/contracts/Customs.sol
@@ -6,6 +6,13 @@ contract Customs {
     mapping (address => Trip[]) public personToHistory;
     mapping (address => string) public addressToCountry;
 
+    constructor() {
+        addressToCountry[address(0x123)] = "Ukraine";
+        addressToCountry[address(0x124)] = "Poland";
+        addressToCountry[address(0x125)] = "USA";
+        addressToCountry[address(0x126)] = "Australia";
+    }
+
     function crossBorder(address _from, address _to, uint256 _date) public {
         personToHistory[msg.sender].push(Trip(_from, _to, _date, true));
     }
