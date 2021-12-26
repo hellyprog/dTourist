475d426d7dd254f14e350328c12bc8e83e3d63f8
volodymyr.romaniv <v.b.romaniv@gmail.com>
2021-12-26 12:21:50 +0200

    Changed init of date variable

--- contracts/Customs.sol
+++ contracts/Customs.sol
@@ -6,10 +6,10 @@ contract Customs {
     struct Trip {
         address from;
         address to;
-        uint256 date;
+        uint64 date;
         bool success;
     }
-    
+
     mapping(address => Trip[]) public personToHistory;
     mapping(address => string) public addressToCountry;
 
@@ -17,16 +17,19 @@ contract Customs {
         initializeCountryMapping();
     }
 
-    function crossBorder(address _from, address _to, uint256 _date) public {
-        personToHistory[msg.sender].push(Trip(_from, _to, _date, true));
+    function crossBorder(address _from, address _to) public {
+        personToHistory[msg.sender].push(
+            Trip(_from, _to, uint64(block.timestamp), true)
+        );
     }
 
-    function getValueAtHistoryMapping(address userAddress)  public  view  returns(Trip[] memory) {
-      return personToHistory[userAddress];
+    function getValueAtHistoryMapping(address userAddress) public view returns (Trip[] memory)
+    {
+        return personToHistory[userAddress];
     }
 
     function initializeCountryMapping() private {
-        addressToCountry[address(0x123)] = "Ukraine";   
+        addressToCountry[address(0x123)] = "Ukraine";
         addressToCountry[address(0x124)] = "Poland";
         addressToCountry[address(0x125)] = "USA";
         addressToCountry[address(0x126)] = "Australia";
