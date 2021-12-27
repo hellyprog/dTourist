29649350ece972b5a9be1fab6e932b57e909a3fa
volodymyr.romaniv <v.b.romaniv@gmail.com>
2021-12-27 16:13:35 +0200

    Added home page stylings

--- d-tourist-fe/src/app/shared/shared.module.ts
+++ d-tourist-fe/src/app/shared/shared.module.ts
@@ -1,9 +1,9 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import {RouterModule} from '@angular/router';
+import { MatIconModule } from '@angular/material/icon';
+import { RouterModule } from '@angular/router';
 import { HeaderComponent } from './header/header.component';
 import { FooterComponent } from './footer/footer.component';
-
 
 
 @NgModule({
@@ -13,7 +13,8 @@ import { FooterComponent } from './footer/footer.component';
   ],
   imports: [
     CommonModule,
-    RouterModule
+    RouterModule,
+    MatIconModule
   ],
   exports: [
     HeaderComponent,
