be81b9932181d71c212a8ac230e8f03a8f4643a6
volodymyr.romaniv <v.b.romaniv@gmail.com>
2022-02-20 13:45:25 +0200

    Added copying to clipboard contract address

--- d-tourist-fe/src/app/insurance-management/insurance-management.component.ts
+++ d-tourist-fe/src/app/insurance-management/insurance-management.component.ts
@@ -7,10 +7,20 @@ import { Component, OnInit } from '@angular/core';
 })
 export class InsuranceManagementComponent implements OnInit {
   contractBalance = 100;
-
+  contractAddress = '0x26287D7ff0A24DB1B54b6Bb04c8558b197297e85';
   constructor() { }
 
   ngOnInit(): void {
   }
 
+  copyContractAddress() {
+    if (!navigator.clipboard) {
+      return;
+    }
+    navigator.clipboard.writeText(this.contractAddress).then(function() {
+      console.log('Async: Copying to clipboard was successful!');
+    }, function(err) {
+      console.error('Async: Could not copy text: ', err);
+    });
+  }
 }
