114c7e3dc40962dc6eace23ef3c63afcdf252c1d
volodymyr.romaniv <v.b.romaniv@gmail.com>
2022-03-18 23:35:57 +0200

    Made scanner route as 'external'

--- d-tourist-fe/src/app/app.component.ts
+++ d-tourist-fe/src/app/app.component.ts
@@ -1,8 +1,20 @@
 import { Component } from '@angular/core';
+import { NavigationEnd, Router } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent { }
+export class AppComponent {
+  isExternalRoute = false;
+
+  constructor(public router: Router) {
+    this.router.events.subscribe((value) => {
+      if (value instanceof NavigationEnd) {
+        const scannerPath = '/ticket-scanner';
+        this.isExternalRoute = value.url === scannerPath || value.urlAfterRedirects === scannerPath;
+      }
+    });
+  }
+}
